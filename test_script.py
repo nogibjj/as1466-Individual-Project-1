5404a1d0bb08ba674e08fccafcafa91b04191e8d
Nastiiasaenko <54864655+Nastiiasaenko@users.noreply.github.com>
2023-09-21 05:22:45 -0400

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,6 +1,6 @@
 from python_script import make_whole_df
 import pandas as pd
-df = read_csv('BQ_data.csv')
+df = pd.read_csv('BQ_data.csv')
 def test_main():
    assert make_whole_df(df).iloc[0,1] == 1501591568
 
