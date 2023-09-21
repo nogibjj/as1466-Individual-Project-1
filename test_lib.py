8e7fb7b53ee397d4eb52f97ed9966ecbc4b1c22d
Nastiiasaenko <54864655+Nastiiasaenko@users.noreply.github.com>
2023-09-21 08:39:11 +0000

    update

--- test_lib.py
+++ test_lib.py
@@ -1,7 +1,36 @@
-from mylib.lib import sum
+
+from mylib.lib import *
+
+import pandas as pd
+
+df = pd.read_csv("BQ_data.csv")
+json_case = df["totals"][0]
+answer = {
+    "visits": "1",
+    "hits": "1",
+    "pageviews": "1",
+    "timeOnSite": None,
+    "bounces": "1",
+    "transactions": None,
+    "transactionRevenue": None,
+    "newVisits": "1",
+    "screenviews": None,
+    "uniqueScreenviews": None,
+    "timeOnScreen": None,
+    "totalTransactionRevenue": None,
+    "sessionQualityDim": "1",
+}
+revenue = pd.read_excel(
+    "Analytics 1 Master View Sales Performance 20230501-20230630.xlsx",
+    sheet_name="Dataset1",
+)
+
 
 def test_main():
-    assert sum(1,2) == 3
-    assert sum(10,10) == 20
+    assert unpack_json(json_case, "totals") == answer
+    ## asserting stats functions
+    assert mean(revenue["Revenue"]) == revenue["Revenue"].mean()
+    assert median(revenue["Revenue"]) == revenue["Revenue"].median()
+
 
 test_main()
