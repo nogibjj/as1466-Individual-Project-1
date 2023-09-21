b4d4b21f9e9bce58e11e7ea1c7ebee1ce420f83f
Nastiiasaenko <54864655+Nastiiasaenko@users.noreply.github.com>
2023-09-21 04:01:31 -0400

    Update create_hidden_html_notebook.py

--- create_hidden_html_notebook.py
+++ create_hidden_html_notebook.py
@@ -3,7 +3,7 @@ from nbconvert import HTMLExporter
 from nbconvert.preprocessors import ExecutePreprocessor
 
 def create_hidden_html_notebook(notebook_path, output_path):
-    with open(notebook_path, 'r', encoding='utf-8') as nb_file:
+    with open('main.ipynb', 'r', encoding='utf-8') as nb_file:
         notebook = nbformat.read(nb_file, as_version=4)
 
     # Create an ExecutePreprocessor and specify the available kernel name
